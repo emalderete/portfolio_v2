985f73ef738bfd226a2d546f657d32e1e3fbcc2d
belenbarrionuevo <nicolas.alderete95@live.com>
2022-04-07 17:05:24 -0300

    Se trabaja sobre el Hero
    Se crea el componente Navbar para ser utilizado en las diferentes páginas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+const Navbar = () => {
+    return (
+        <section>
+            <a name='Home'></a>
+            <div class="header-container-b">
+                <i id="code-logo" class="fa-solid fa-code"></i>
+                <div class="name-container">
+                    <h1 class="name">¡Hola! Soy Emanuel Alderete.<br/>Desarrollador FullStack.</h1>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Navbar;
