4b77eaaec1255838c7d121b03e2727f79b73e054
belenbarrionuevo <nicolas.alderete95@live.com>
2022-04-07 17:13:30 -0300

    Creada nueva página llamada About.js
    Se crea la página About para colocar la información personal en la misma de una forma más cómoda.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import About from './Pages/About';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/about' element={<About></About>}></Route>
         </Routes>
       </Router>
     </div>
