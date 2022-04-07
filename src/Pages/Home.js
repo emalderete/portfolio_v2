0efc9a73032805aa47b03ee87362d7bc2aa4463c
belenbarrionuevo <nicolas.alderete95@live.com>
2022-04-07 17:08:15 -0300

    Creada página Home
    • Se crea la página Home la cual será la primera vista del proyecto.
    • Se elimina la imagen de favicon.ico por defecto del proyecto y se agrega una nueva imagen svg para dicho propósito.
    • Se vincula la biblioteca de iconos Fontawesome con el proyecto mediante etiqueta "script" en el archivo index.html lo cual no aparecerá como dependencia en "package.json"

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import '../App.css';
+
+const Home = () => {
+    return (
+        <section>
+            <Navbar></Navbar>
+        </section>
+    );
+};
+
+export default Home;
