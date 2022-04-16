e513c09c84a58bd776d374008befe764e6bbad31
emalderete <nicolas.alderete95@live.com>
2022-04-16 18:51:11 -0300

    Maquetada sección de proyectos
    • Se corrige posición del elemento "MoreInfoText" en el html.
    • Se agregan los estilos necesarios para mejorar su estética.

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ImgProject1of2 from '../Img/image/project1-2.jpg';
+import ImgProject1Image2 from '../Img/image/project1-2.jpg';
+import ImgProject2Image1 from '../Img/image/project2-1.jpg';
+import ImgProject3Image1 from '../Img/image/project3-1.jpg';
 
 const Portfolio = () => {
     return (
@@ -11,14 +13,47 @@ const Portfolio = () => {
                 </div>
                 <div className='projectsContainer'>
                     <div className='projects'>
-                        <div className='projectMargin'>
+                        <div className='projectsMargin'>
                             <div className='projectsImgMainContainer'>
-                                <img className='projectsImgMain' src={ImgProject1of2} alt=''></img>
+                                <img className='projectsImgMain' src={ImgProject1Image2} alt=''></img>
+                            </div>
+                            <div className='projectsTextContainer'>
+                                <div>
+                                    <h3 className='projectsTitle'>Blog sobre programación</h3>
+                                    <p className='projectsText'>Se trata de un proyecto evaluativo que he realizado en equipo con algunos compañeros del instituto.</p>
+                                </div>
                             </div>
                         </div>
+                        <p className='projectsMoreInfoText'>Toca para más info...</p>
                     </div>
-                    <div className="projects"></div>
-                    <div className="projects"></div>
+                    <div className='projects'>
+                        <div className='projectsMargin'>
+                            <div className='projectsImgMainContainer'>
+                                <img className='projectsImgMain' src={ImgProject2Image1} alt=''></img>
+                            </div>
+                            <div className='projectsTextContainer'>
+                                <div>
+                                    <h3 className='projectsTitle'>Trabajo Práctico Steam Rolling Code</h3>
+                                    <p className='projectsText'>Proyecto evaluativo realizado con compañeros de equipo como exámen final integrador. Basado en el sitio web Steam.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <p className='projectsMoreInfoText'>Toca para más info...</p>
+                    </div>
+                    <div className="projects">
+                        <div className='projectsMargin'>
+                            <div className='projectsImgMainContainer'>
+                                <img className='projectsImgMain' src={ImgProject3Image1} alt=''></img>
+                            </div>
+                            <div className='projectsTextContainer'>
+                                <div>
+                                    <h3 className='projectsTitle'>Curriculum Vitae versión web</h3>
+                                    <p className='projectsText'>Mi propio curriculum de experiencias laborales y estudios académicos pero en versión web.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <p className='projectsMoreInfoText'>Toca para más info...</p>
+                    </div>
                 </div>
             </article>
         </section>
