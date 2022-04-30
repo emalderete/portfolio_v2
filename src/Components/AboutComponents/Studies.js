85d34f83bef63167cddffca3f2519c7bd11c17e3
emalderete <nicolas.alderete95@live.com>
2022-04-30 14:20:13 -0300

    Se crea componente Studies y más...
    • Creado componente "Studies.js" para renderizar información personal.
    • Se agrega animación al Navbar de la página principal.

--- src/Components/AboutComponents/Studies.js
+++ src/Components/AboutComponents/Studies.js
@@ -7,8 +7,8 @@ const Studies = () => {
             <article className='studies'>
                 <p className='infoText'>
                     A lo largo de mi vida he estudiado en diferentes lugares, algunas fueron carreras inconclusas, he cursado diferentes asignaciones pero ninguna ha despertado tanto mi pasión como lo ha hecho la programación y desarrollo informático.<br />Mi
-                    principal formación fue en <a class="links" href="https://rollingcodeschool.com" target="blank" rel="noopener noreferrer">Rolling Code School</a> donde aprendí desarrollo FullStack y las tecnologías de HTML 5 <i class="fa-brands fa-html5"></i>,
-                    CSS 3 <i class="fa-brands fa-css3"></i>, JavaScript <i class="fa-brands fa-js-square "></i> y React <i class="fa-brands fa-react"></i>, entre otras tecnologías. En ese momento sentí que descubrí un nuevo mundo que antes estaba oculto ante mis ojos, la
+                    principal formación fue en <a className='links' href='https://rollingcodeschool.com' target='blank' rel='noopener noreferrer'>Rolling Code School</a> donde aprendí desarrollo FullStack y las tecnologías de HTML 5 <i className='fa-brands fa-html5'></i>,
+                    CSS 3 <i className='fa-brands fa-css3'></i>, JavaScript <i className='fa-brands fa-js-square'></i> y React <i className='fa-brands fa-react'></i>, entre otras tecnologías. En ese momento sentí que descubrí un nuevo mundo que antes estaba oculto ante mis ojos, la
                     programación es algo que todos deberían aprender en algún momento de su vida.
                 </p>
                 <h2 className='infoTitle'>¿Dónde estudié?</h2>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 
 const Navbar = () => {
+    const [tracker, setTracker] = useState();
+
+    useEffect(()=>{   
+        window.addEventListener('scroll', activeFunctions);
+        
+        function activeFunctions(){
+            scrollTracker();
+        };
+        
+        function scrollTracker(){
+            const scroll = window.scrollY;
+            console.log(scroll);
+            setTracker(scroll);
+        };
+        
+    }, []);
+
     return (
-        <div className='navbar'>
-            <div className='navbarBrand effectOpacity'>
-                <i className='fa-solid fa-code codeMinilogo'></i>
-                <h2 className='miniName effectSpacingLetter'>Emanuel Alderete</h2>
+        <div>
+            <div className={tracker >= 288 ? 'phantomContainer' : 'displayNone phantomContainer'}></div>
+            <div className={tracker >= 288 ? 'navbar fixedTop' : 'navbar'}>
+                <div className={tracker >= 288 ? 'navbarBrand' : 'navbarBrand effectOpacity'}>
+                    <i className='fa-solid fa-code codeMinilogo'></i>
+                    <h2 className={tracker >= 288 ? 'miniName' : 'miniName effectSpacingLetter'}>Emanuel Alderete</h2>
+                </div>
+                <ul className='navigation'> 
+                    <li className='navActived'><a className="itemHome" href="#home">Home</a></li>
+                    <li className=''><a className="itemPort" href="#portfolio">Portfolio</a></li>
+                    <li className=''><a className="itemSkill" href="#habilidades">Habilidades</a></li>
+                    <li className=''><Link className="item" to='/about'>Acerca de mi <i className="fa-solid fa-square-arrow-up-right"></i></Link></li>
+                    <li className=''><Link className="item" to='/contact'>Contacto <i className="fa-solid fa-square-arrow-up-right"></i></Link></li>
+                </ul>
             </div>
-            <ul className='navigation'> 
-                <li className='navActived'><a className="itemHome" href="#home">Home</a></li>
-                <li className=''><a className="itemPort" href="#portfolio">Portfolio</a></li>
-                <li className=''><a className="itemSkill" href="#habilidades">Habilidades</a></li>
-                <li className=''><Link className="item" to='/about'>Acerca de mi <i className="fa-solid fa-square-arrow-up-right"></i></Link></li>
-                <li className=''><Link className="item" to='/contact'>Contacto <i className="fa-solid fa-square-arrow-up-right"></i></Link></li>
-            </ul>
         </div>
     );
 };
