b45b8e655a3c65a9368e289f6dbfa6a639a929bc
emalderete <nicolas.alderete95@live.com>
2022-05-24 20:12:03 -0300

    Cambios en la administración de imágenes del componente Portfolio.js
    • Se eliminan las importaciones de imágenes que se hacían al principio del código.
    En su lugar se subieron las mismas a un server y se utilizan las URLs para renderizarlas.
    • Se comentan las líneas de código que renderizan la sección multimedia dentro de la ventana modal.

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,16 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ImgProject2Image1 from '../Img/image/project2-1.jpg';
-import ImgProject3Image1 from '../Img/image/project3-1.jpg';
-import project1_1 from '../Img/image/project1-1.jpg';
-import project1_2 from '../Img/image/project1-2.jpg';
-import project1_3 from '../Img/image/project1-3.jpg';
-import project1_4 from '../Img/image/project1-4.jpg';
-import project2_1 from '../Img/image/project2-1.jpg';
-import project2_2 from '../Img/image/project2-2.jpg';
-import project2_3 from '../Img/image/project2-3.jpg';
-import project2_4 from '../Img/image/project2-4.jpg';
-import project3_1 from '../Img/image/project3-1.jpg';
-import { projects } from './Data/ProjectsData';
+import { projects, DataProject } from './Data/ProjectsData';
 import '../App.css';
 
 const Portfolio = () => {
@@ -86,29 +75,17 @@ const Portfolio = () => {
                     <hr className='modalSeparator'/>
                     <div className='modalContent'>
                         <div className='modalGallery'>
-                            <ul className='modalGallerySlider'>
-                                <li id='slide1'><img src={modalInfo[2]} alt='' /></li>
-                                <li id='slide2'><img src={modalInfo[3]} alt='' /></li>
-                                <li id='slide3'><img src={modalInfo[4]} alt='' /></li>
-                                <li id='slide4'><img src={modalInfo[5]} alt='' /></li>
-                            </ul>
-                            <ul className='modalGalleryPreviewer'>
-                                <li><a href='#slide1'><img src={modalInfo[2]} alt='' /></a></li>
-                                <li><a href='#slide2'><img src={modalInfo[3]} alt='' /></a></li>
-                                <li><a href='#slide3'><img src={modalInfo[4]} alt='' /></a></li>
-                                <li><a href='#slide4'><img src={modalInfo[5]} alt='' /></a></li>
-                            </ul>
+                            {conditionalRender()}
                         </div>
                         <div className='modalInfo'>
                             <div className='modalInfoTextContainer'>
                                 <h3 className='modalSubtitle'>Descripción:</h3>
-                                <p className='modalText'>{modalInfo[1]}</p>
+                                <p className='modalText'>{modalInfo.fullDesc}</p>
                              </div>
                             <div className='modalButtons'>
-                                <a className='modalSeePage' href={modalInfo[6]} rel='noreferrer noopener' target='_blank'>
+                                <a className='modalSeePage' href={modalInfo.linkPage} rel='noreferrer noopener' target='_blank'>
                                     Ver sitio <i className="fa-solid fa-square-arrow-up-right"></i>
                                 </a>
-                                <a onMouseOver={mouseOver} onMouseOut={mouseOut} className='modalSeeCode' href={modalInfo[8]} rel='noreferrer noopener' target='_blank'>
                                     <div style={gitHover ? {width : '9.8rem'} : null} className='modalPositionIconGit'>
                                         <i className='fa-brands modalIconGit fa-github-square'></i>
                                         <p className={gitHover ? 'modalIconGitText' : 'modalIconGitText effectOpacity'}>Ver código</p>
