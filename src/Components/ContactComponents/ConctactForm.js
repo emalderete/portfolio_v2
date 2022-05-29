60b4f1e3d1dcdd8beda97c9d2f7c73df8e3bbec7
emalderete <nicolas.alderete95@live.com>
2022-05-29 16:52:26 -0300

    Corregido error de animación en formulario.
    • Se crea una función que, al perder foco del input, evalúe si el mismo contiene algún valor, y de ser así sobrescribe el estado de dicho input.
    • Se elimina manifiesto que generaba un ligero error en la consola.

--- src/Components/ContactComponents/ConctactForm.js
+++ src/Components/ContactComponents/ConctactForm.js
@@ -11,6 +11,15 @@ const ConctactForm = () => {
         span : {transform : 'translate(-1rem, -1rem)', fontSize : '100%'}
     };
 
+    function evaluateInputState(element, state){
+        let element_data = document.querySelector(element);
+        if (element_data.value.length > 0){
+            state(true);
+        } else {
+            state(false);
+        }
+    };
+
     return (
         <section>
             <div className='shapeTop'></div>
@@ -20,19 +29,19 @@ const ConctactForm = () => {
                     <form className='contactForm'>
                         <label className='contactFormLabel' htmlFor='name'>
                             <span style={nameState ? formStyles.span : null} className='spanInput'>¿Cuál es tu nombre?</span>
-                            <input className='formInput' typeof='text' name='name' id='name' required onFocus={()=>{setNameState(true)}} onBlur={()=>{setNameState(false)}}/>
+                            <input className='formInput' typeof='text' name='name' id='name' required onFocus={()=>{setNameState(true)}} onBlur={()=>{evaluateInputState('#name', setNameState)}}/>
                         </label>
                         <label className='contactFormLabel' htmlFor='mail'>
                             <span style={mailState ? formStyles.span : null} className='spanInput'>Correo electrónico</span>
-                            <input className='formInput' typeof='email' name='email' id='email' required onFocus={()=>{setMailState(true)}} onBlur={()=>{setMailState(false)}}/>
+                            <input className='formInput' typeof='email' name='email' id='email' required onFocus={()=>{setMailState(true)}} onBlur={()=>{evaluateInputState('#email', setMailState)}}/>
                         </label>
                         <label className='contactFormLabel' htmlFor='phone'>
                             <span style={phoneState ? formStyles.span : null} className='spanInput'>Número de teléfono</span>
-                            <input className='formInput' typeof='number' name='phone' id='phone' required onFocus={()=>{setPhoneState(true)}} onBlur={()=>{setPhoneState(false)}}/>
+                            <input className='formInput' typeof='number' name='phone' id='phone' required onFocus={()=>{setPhoneState(true)}} onBlur={()=>{evaluateInputState('#phone', setPhoneState)}}/>
                         </label>
                         <label className='contactFormLabel' htmlFor='message'>
                             <span style={messageState ? formStyles.span : null} className='spanInput'>Escribe tu mensaje</span>
-                            <textarea className='formInput formInputTextArea' typeof='text' name='message' id='message' required onFocus={()=>{setMessageState(true)}} onBlur={()=>{setMessageState(false)}}/>
+                            <textarea className='formInput formInputTextArea' typeof='text' name='message' id='message' required onFocus={()=>{setMessageState(true)}} onBlur={()=>{evaluateInputState('#message', setMessageState)}}/>
                         </label>
                     </form>
                 </article>
